e8ccb29189d33390601a5e66f17dea35c1f7341b
Johnn3volta <Johnn3volta@gmail.com>
2021-05-31 14:52:41 +0300

    Keep calm and commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-function f()
-
-{
-  console.log('Hello wpld') // FIXME: Delete before push
+function f() {
+  console.log('Hello wpld'); // FIXME: Delete before push
 }
 
+console.log('Ho'); // FIXME: Delete before push
 
-console.log('Ho') // FIXME: Delete before push
+function f1() {
+  console.log('www'); // FIXME: Delete before push
+}
+
+f1();
