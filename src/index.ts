478e5219e62304bd18b96213f41f7f00b7ee9841
Johnn3volta <Johnn3volta@gmail.com>
2021-05-31 15:03:34 +0300

    try

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-function f() {
-  console.log('Hello wpld'); // FIXME: Delete before push
+function f()
+
+{
+  console.log('Hello wpld') // FIXME: Delete before push
 }
 
-console.log('Ho'); // FIXME: Delete before push
+
+console.log('Ho') // FIXME: Delete before push
+
 
 function f1() {
-  console.log('www'); // FIXME: Delete before push
+  console.log(
+    'www'
+  ) // FIXME: Delete before push
 }
 
-f1();
+f1()
