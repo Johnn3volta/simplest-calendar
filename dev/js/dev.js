dc5bec82bb3f24f0088b0c9880c48a31d441c9ca
Johnn3volta <Johnn3volta@gmail.com>
2021-06-01 11:54:45 +0300

    Working with start config

---
+++ dev/js/dev.js
@@ -0,0 +1,9 @@
+console.log('wadwa'); // FIXME: Delete before push
+
+function hello() {
+  console.log('Hello nMwwwwwwwwwwAwwadawe'); // FIXME: Delete before push
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+hello();
