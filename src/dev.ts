33678c88937579a5be93fac009e1541a026928c4
Johnn3volta <Johnn3volta@gmail.com>
2021-06-02 17:07:36 +0300

    add develop branch

--- src/dev.ts
+++ src/dev.ts
@@ -1,9 +1,3 @@
-console.log('wadwa'); // FIXME: Delete before push
-
-function hello() {
-  console.log('Hello nMwwwwwwwwwwAwwadawe'); // FIXME: Delete before push
-}
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-hello();
+// import f from './index';
+//
+// f();
